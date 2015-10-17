361e8bf7ea2530cc4a6c0316e17ea3c193eea976
coldBl00d <akhilrajr94@gmail.com>
2015-10-17 13:33:54 +0530

    changes in the readme

--- src/gui/SearchGUI.java
+++ src/gui/SearchGUI.java
@@ -5,10 +5,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+import entities.Tag;
+import helpers.DatabaseHelper;
+import testdriveClasses.Program;
+
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import java.awt.Font;
 import java.awt.event.ActionListener;
+import java.awt.event.TextEvent;
+import java.awt.event.TextListener;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import javax.swing.JMenuBar;
 import java.awt.Color;
@@ -21,72 +31,77 @@ public class SearchGUI {
 	private JFrame frame;
 	private JTextField searchTerm;
 	private final String title = "Main GUI";
-	private String [] tagList;
-	private String [] searchResults;
-	JScrollPane jscrlPane;
-	
-	SpringLayout springLayout;
+	private ArrayList<Tag> tagList;
+	private String[] searchResults;
+	private JScrollPane jscrlPane;
+	private JButton searchButton;
+	private SpringLayout springLayout;
 	boolean displayFileDetail = true;
-	JLabel labelNameConst;
-	JLabel labelSizeConst;
-	
-	
-
-	
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					SearchGUI window = new SearchGUI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private JLabel labelNameConst;
+	private JLabel labelSizeConst;
+	private JComboBox<String> tagCombo;
+	private JList searchResultList;
+	EventListeners eventListeners;
+	JButton runButton;
+	JButton openLocationButton;
 
 	public SearchGUI() {
-		initialize();
+		eventListeners = new EventListeners();
+		EventQueue.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				initialize();
+				
+				
+			}
+		});
+		
 	}
 
 	private void initialize() {
 		frame = new JFrame();
+		springLayout = new SpringLayout();
+		searchTerm = new JTextField();
+		searchButton = new JButton("Search");
+		tagCombo = new JComboBox <String> ();
+		
+		
+		/*****FRAME*****/
 		frame.getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 16));
 		frame.setBounds(100, 100, 800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		springLayout = new SpringLayout();
 		frame.getContentPane().setLayout(springLayout);
 		frame.setTitle(title);
 		
-		searchTerm = new JTextField();
+		/*****SEARCH TEXTBOX*****/
 		springLayout.putConstraint(SpringLayout.SOUTH, searchTerm, -476, SpringLayout.SOUTH, frame.getContentPane());
 		searchTerm.setFont(new Font("Tahoma", Font.BOLD, 16));
 		springLayout.putConstraint(SpringLayout.WEST, searchTerm, 22, SpringLayout.WEST, frame.getContentPane());
 		springLayout.putConstraint(SpringLayout.EAST, searchTerm, -29, SpringLayout.EAST, frame.getContentPane());
-		searchTerm.setToolTipText("");
+		searchTerm.setToolTipText("Enter File Name Here");
 		frame.getContentPane().add(searchTerm);
 		searchTerm.setColumns(10);
-		
-		JButton searchButton = new JButton("Search");
+		searchTerm.getDocument().addDocumentListener(eventListeners);
+		/*****SEARCH BUTTON*****/
 		springLayout.putConstraint(SpringLayout.NORTH, searchButton, 21, SpringLayout.SOUTH, searchTerm);
 		springLayout.putConstraint(SpringLayout.SOUTH, searchButton, -426, SpringLayout.SOUTH, frame.getContentPane());
 		springLayout.putConstraint(SpringLayout.EAST, searchButton, -177, SpringLayout.EAST, frame.getContentPane());
-		searchButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				searchAction();
-			}
-		});
 		frame.getContentPane().add(searchButton);
+		searchButton.addActionListener(eventListeners);
 		
-		JComboBox tagCombo = new JComboBox ();
+		/*****TAG COMBOBOX*****/
 		springLayout.putConstraint(SpringLayout.WEST, searchButton, 154, SpringLayout.EAST, tagCombo);
 		springLayout.putConstraint(SpringLayout.NORTH, tagCombo, 21, SpringLayout.SOUTH, searchTerm);
 		springLayout.putConstraint(SpringLayout.SOUTH, tagCombo, -426, SpringLayout.SOUTH, frame.getContentPane());
 		springLayout.putConstraint(SpringLayout.WEST, tagCombo, 0, SpringLayout.WEST, searchTerm);
 		springLayout.putConstraint(SpringLayout.EAST, tagCombo, -468, SpringLayout.EAST, frame.getContentPane());
 		frame.getContentPane().add(tagCombo);
+		tagList = Program.databaseHelper.getTagList();
+		this.setTagComboBox(tagList);
 		
+		/*****MENU BAR*****/
 		JMenuBar menuBar = new JMenuBar();
 		springLayout.putConstraint(SpringLayout.NORTH, searchTerm, 22, SpringLayout.SOUTH, menuBar);
 		menuBar.setBackground(new Color(128, 128, 128));
@@ -95,9 +110,9 @@ public class SearchGUI {
 		springLayout.putConstraint(SpringLayout.EAST, menuBar, 782, SpringLayout.WEST, frame.getContentPane());
 		frame.getContentPane().add(menuBar);
 		
-		JList searchResultList = new JList(); //Add the array here 
-		
-		
+		/*****JSCROLL PANE WITH SEARCH RESULT*****/
+		searchResultList = new JList();
+		searchResultList.setFont(new Font("Tahoma", Font.ITALIC, 20));
 		jscrlPane = new JScrollPane (searchResultList);
 		jscrlPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		jscrlPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
@@ -107,35 +122,28 @@ public class SearchGUI {
 		springLayout.putConstraint(SpringLayout.EAST,jscrlPane,0,SpringLayout.EAST,tagCombo);
 		frame.getContentPane().add(jscrlPane);
 		
-		searchResultList.setFont(new Font("Tahoma", Font.ITALIC, 20));
-			
 		
-			setFileDetails("Prince Of Persia","C:\\ProgramFiles\\POP","8Gb");
+		/****Implement SetFileDetail() Before Run Button and Open Location button is set*****/
+		/*****RUN BUTTON****//*
+		runButton = new JButton("Run");
+		springLayout.putConstraint(SpringLayout.NORTH, runButton, 51, SpringLayout.SOUTH, labelSizeConst);
+		springLayout.putConstraint(SpringLayout.WEST, runButton, 154, SpringLayout.EAST, jscrlPane);
+		springLayout.putConstraint(SpringLayout.SOUTH, runButton, -202, SpringLayout.SOUTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, runButton, -219, SpringLayout.EAST, frame.getContentPane());
+		frame.getContentPane().add(runButton);
 		
-			JButton btnOpen = new JButton("Run");
-			springLayout.putConstraint(SpringLayout.NORTH, btnOpen, 51, SpringLayout.SOUTH, labelSizeConst);
-			springLayout.putConstraint(SpringLayout.WEST, btnOpen, 154, SpringLayout.EAST, jscrlPane);
-			springLayout.putConstraint(SpringLayout.SOUTH, btnOpen, -202, SpringLayout.SOUTH, frame.getContentPane());
-			springLayout.putConstraint(SpringLayout.EAST, btnOpen, -219, SpringLayout.EAST, frame.getContentPane());
-			frame.getContentPane().add(btnOpen);
-			
-			JButton btnOpenLocation = new JButton("Open Location");
-			springLayout.putConstraint(SpringLayout.NORTH, btnOpenLocation, 0, SpringLayout.NORTH, btnOpen);
-			springLayout.putConstraint(SpringLayout.WEST, btnOpenLocation, 42, SpringLayout.EAST, btnOpen);
-			frame.getContentPane().add(btnOpenLocation);		
+		*//*****OPEN LOCATION BUTTON*****//*
+		openLocationButton = new JButton("Open Location");
+		springLayout.putConstraint(SpringLayout.NORTH, openLocationButton, 0, SpringLayout.NORTH, runButton);
+		springLayout.putConstraint(SpringLayout.WEST, openLocationButton, 42, SpringLayout.EAST, runButton);
+		frame.getContentPane().add(openLocationButton);*/
+		
+		if (frame != null)
+			frame.setVisible(true);
 	}
-	
-	/*
-	 * DBMS Fetch here 
-	 * return corresponding boolean to denote if search is successful 
-	 * or not
-	 */
+
 	private boolean searchAction (){
 		return true;
-	}
-	
-	public void setTagArray(String [] tagList){
-		this.tagList= tagList;
 	}
 	
 	public void setFileDetails (String fileName,String filePath,String fileSize){
@@ -152,7 +160,7 @@ public class SearchGUI {
 		labelPathConst.setFont(new Font("Tahoma", Font.BOLD, 19));
 		frame.getContentPane().add(labelPathConst);
 		
-		 labelSizeConst = new JLabel("Size   :");
+		labelSizeConst = new JLabel("Size   :");
 		springLayout.putConstraint(SpringLayout.NORTH, labelSizeConst, 19, SpringLayout.SOUTH, labelPathConst);
 		springLayout.putConstraint(SpringLayout.WEST, labelSizeConst, 155, SpringLayout.EAST, jscrlPane);
 		labelSizeConst.setFont(new Font("Tahoma", Font.BOLD, 19));
@@ -175,6 +183,52 @@ public class SearchGUI {
 		springLayout.putConstraint(SpringLayout.SOUTH, label, 0, SpringLayout.SOUTH, labelSizeConst);
 		springLayout.putConstraint(SpringLayout.WEST, label, 31, SpringLayout.EAST, labelSizeConst);
 		frame.getContentPane().add(label);
+		
+	}
+	
+	private void setTagComboBox (ArrayList<Tag> tagArray){
+		String[] tagNameArray= new String[tagArray.size()];
+		int index = 0;
+		tagCombo.removeAll();
+		for (Tag i : tagArray){
+			System.out.println("Adding Tag Items To TagComboBox");
+			tagCombo.addItem(i.getTagName());
+		}
+		
+	}
+	
+	private class EventListeners implements ActionListener, DocumentListener{
+
+		String searchString = null;
+		Object source;
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			source = e.getSource();
+			if (source==searchButton){
+				System.out.println("Button Pressed :"+searchString);
+			}
+			else if (source==runButton){
+				
+			}
+			else if (source==openLocationButton) {
+				
+			}
+			
+		}
+
+		@Override
+		public void changedUpdate(DocumentEvent e) {}
+
+		@Override
+		public void insertUpdate(DocumentEvent e) {
+			searchString = searchTerm.getText();			
+		}
+
+		@Override
+		public void removeUpdate(DocumentEvent e) {
+			searchString = searchTerm.getText();	
+		}
 		
 	}
 }
--- src/helpers/DatabaseHelper.java
+++ src/helpers/DatabaseHelper.java
@@ -11,8 +11,9 @@ public class DatabaseHelper {
 
 
 	static final String JDBC_DRIVER="com.mysql.jdbc.Driver";
-	static final String DB_URL="jdbc:mysql://localhost/FILE_TRACKER";
-
+	//static final String DB_URL="jdbc:mysql://localhost/FILE_TRACKER";
+	static final String DB_URL="jdbc:mysql://localhost:3305/file_tracker";
+	
 	//database credentials
 	static final String USER="root";
 	static final String PASS="asdf1234";
@@ -20,7 +21,6 @@ public class DatabaseHelper {
 	protected Connection connection= null;
 	
 	/**
-	 * 
 	 * @throws SQLException
 	 * @throws ClassNotFoundException
 	 */
@@ -28,7 +28,6 @@ public class DatabaseHelper {
 	public void initialize() throws SQLException, ClassNotFoundException {
 
 		Class.forName(JDBC_DRIVER);
-
 		System.out.println("connecting to database..");
 		connection= DriverManager.getConnection(DB_URL, USER, PASS);
 		System.out.println("connection established...");
